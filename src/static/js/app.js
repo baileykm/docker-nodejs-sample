7f1368af64ff91a6f5e9098cc4dfd65f9ea81290
Bailey <mr.bailey@163.com>
2024-06-20 02:09:16 +0800

    my commit

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Item'}
+                        {submitting ? 'Adding...' : 'Add Items'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
